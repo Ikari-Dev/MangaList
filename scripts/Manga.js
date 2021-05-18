508363fc18983464eae3a733612c97473575c9ff
DelcroixFlorian <florian.delcroix98@gmail.com>
2021-05-19 01:23:24 +0200

    Init

---
+++ scripts/Manga.js
@@ -0,0 +1,8 @@
+class Manga {
+    constructor(name, description, img, wikilink) {
+        this.name = name
+        this.description = description
+        this.img = img
+        this.wikilink = wikilink
+    }
+}
---
+++ scripts/index.js
@@ -0,0 +1,115 @@
+let list = []
+list.push(new Manga("One piece", "One Piece est une série de mangas shōnen créée par Eiichirō Oda.","../asset/op.jpg","https://fr.wikipedia.org/wiki/One_Piece"))
+list.push(new Manga("Bleach", "Bleach (BLEACH, ブリーチ, Burīchi) est un shōnen manga écrit et illustré par Tite Kubo.","../asset/bleach.jpg","https://fr.wikipedia.org/wiki/Bleach_(manga)"))
+list.push(new Manga("Naruto", "Naruto (ナルト) est un shōnen manga écrit et dessiné par Masashi Kishimoto", "../asset/naruto.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Red eyes Sword", "Red Eyes Sword: Akame ga Kill! (アカメが斬る!, Akame ga Kiru!?, littéralement Red Eye Killer ou Akame Slashes!) est une série de mangas écrite par Takahiro et illustrée par Tetsuya Tashiro", "../asset/akame.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Red eyes Sword zero", "Red Eyes Sword: Akame ga Kill! zero (アカメが斬る!, Akame ga Kiru!?, littéralement Red Eye Killer ou Akame Slashes!) est une série de mangas écrite par Takahiro et illustrée par Tetsuya Tashiro", "../asset/akamezero.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Blame", "BLAME! (ブラム!, Buramu!?) est une série de seinen manga cyberpunk écrit et dessiné par Tsutomu Nihei", "../asset/blame.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Attaque des titans", "Naruto (ナルト) est un shōnen manga écrit et dessiné par Masashi Kishimoto", "../asset/snk.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Dragon Ball", "Dragon Ball (ドラゴンボール, Doragon Bōru?, litt. Dragon Ball) est une série de mangas créée par Akira Toriyama,", "../asset/dball.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Dragon Ball Z", "Dragon Ball (ドラゴンボール, Doragon Bōru?, litt. Dragon Ball) est une série de mangas créée par Akira Toriyama,", "../asset/dbz.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Ajin", "Ajin : Semi-humain (亜人, Ajin?) est un seinen manga de Gamon Sakurai.", "../asset/ajin.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Tokyo Ghoul", "Tokyo Ghoul (東京喰種トーキヨーグール, Tōkyō Gūru?) est un seinen manga de Sui Ishida", "../asset/tg.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Tokyo Ghoul re", "Tokyo Ghoul re (東京喰種トーキヨーグール, Tōkyō Gūru?) est un seinen manga de Sui Ishida", "../asset/tgre.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Radiant", "Radiant est un manga français créé par Tony Valenteo", "../asset/radiant.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Alice in Borderland", "Alice in Borderland (今際の国のアリス, Imawa no kuni no Arisu?) est un shōnen manga d'Haro Asō", "../asset/AiB.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Promised Neverland", "The Promised Neverland (約束のネバーランド, Yakusoku no Nebārando?) est un shōnen manga écrit par Kaiu Shirai et dessiné par Posuka Demizu", "../asset/Pn.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Psyren", "Psyren (PSYЯEN -サイレン-, Sairen?) est un manga écrit et dessiné par Toshiaki Iwashiro", "../asset/psyren.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Gunnm", "Gunnm (銃夢, Ganmu?, littéralement « rêve d'une arme »)note 1 est une série de seinen manga de Yukito Kishiro", "../asset/gunnm.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Gunnm: Last Order", "Gunnm Last Order (銃夢Last Order, Ganmu Rasuto Ōdā?) est une série de seinen manga de Yukito Kishiro", "../asset/gunnmLO.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Gunnm: Mars Chronicles", "Gunnm Mars Chronicle (銃夢火星戦記, Gunnm: Kasei senki?) est un seinen manga écrit et dessiné par Yukito Kishiro", "../asset/gunnmM.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Solo leveling", "Solo Leveling (hangeul : 나 혼자만 레벨업, RR : Na honjaman lebel-eob) est une série de romans sud-coréens écrite par Chugongi", "../asset/sololeveling.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("TODAG", "Tales of Demons and Gods, abrégée en TODAG, et connue en Chine sous le titre Yao Shen Ji (妖神记), est une série de webnovel écrite par Mad Snail", "../asset/todag.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Biomega", "Biomega (バイオメガ, Baiomega?) est un seinen manga de Tsutomu Nihei", "../asset/biomega.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Berserk", "Berserk (ベルセルク, Beruseruku?) est un manga de dark fantasy de Kentaro Miura", "../asset/berserk.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Knights of Sidonia", "Knights of Sidonia (シドニアの騎士, Shidonia no Kishi?) est un seinen manga écrit et dessiné par Tsutomu Nihei", "../asset/kos.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Nausicaä", "Nausicaä de la Vallée du Vent (風の谷のナウシカ, Kaze no tani no Naushika?) est un manga japonais de Hayao Miyazaki", "../asset/nausicaa.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Goblin Slayer", "Goblin Slayer (ゴブリンスレイヤー, Goburin Sureiyā?), abrégée en GobuSure (ゴブスレ?)1, est une série de light novel japonais écrite par Kumo Kagyū et illustrée par Noboru Kannatsuki", "../asset/GSlayer.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("My Hero Academia", "My Hero Academia (僕のヒーローアカデミア, Boku no Hīrō Akademia?) est un shōnen manga écrit et dessiné par Kōhei Horikoshi", "../asset/mha.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Green World Z", "Green World Z est un manga de Yûsuke Ôsawa", "../asset/GWZ.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("NOiSE", "NOiSE (ノイズ, Noizu?) est un manga créé par Tsutomu Nihei", "../asset/noise.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Abara", "Abara (アバラ?) est un manga en 2 volumes de Tsutomu Nihei", "../asset/abara.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Aposimz", "Aposimz (stylisé comme APOSIMZ) (japonais: 人形 の 国, Hepburn: Ningyō no Kuni) est une série de mangas japonais écrite et illustrée par Tsutomu Nihei", "../asset/aposimz.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Fairy Tail", "Fairy Tail (フェアリーテイル, Fearī Teiru?, littéralement « Queue de fée ») est un shōnen manga écrit et dessiné par Hiro Mashima", "../asset/FT.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Eden Zero", "Edens Zero (エデンズゼロ, Edenzu Zero?) est un shōnen manga écrit et dessiné par Hiro Mashima", "../asset/edenzero.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Green Mechanic", "Green Mechanic4 (Ki-oon) de Yami Shina", "../asset/greenM.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Seven deadly Sin", "Seven Deadly Sins (七つの大罪, Nanatsu no taizai?, litt. « Sept péchés capitaux ») est un shōnen manga écrit et dessiné par Nakaba Suzuki", "../asset/seven.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Death Note", "Death Note (デスノート, Desu Nōto?, littéralement Cahier de la Mort) est un shōnen manga écrit par Tsugumi Ōba et dessiné par Takeshi Obata", "../asset/DNote.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Sword Art Online", "Sword Art Online (ソードアート・オンライン, Sōdo Āto Onrain?) est un light novel écrit par Reki Kawahara", "../asset/sao.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Accel World", "Accel World (アクセル・ワールド, Akuseru Wārudo?) est un light novel écrit par Reki Kawahara et illustré par HiMA", "../asset/accel.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Darwin's Game", "Darwin's Game (ダーウィンズゲーム, Dāwinzugēmu?, litt. « Le Jeu de Darwin ») est un shōnen manga de FLIPFLOPs (duo composé de Shu Miyama pour le scénario et de Yuki Takahata pour le dessin1)", "../asset/darwin.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Btooom", "Btooom! (ブトゥーム!, Butūmu!), stylisé BTOOOM!, est un manga écrit et dessiné par Junya Inoue", "../asset/btoom.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Dead Tube", "Dead Tube est un seinen manga écrit par Mikoto Yamaguchi et dessiné par Touta Kitakawa", "../asset/deadT.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("DeadMan Wonderland", "Deadman Wonderland (デッドマンワンダーランド, Deddoman Wandārando?) est un shōnen manga écrit par Jinsei Kataoka et illustré par Kazuma Kondou", "../asset/deadman.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Doubt", "Doubt (-ダウト-, Dauto?), également appelé Rabbit Doubt (ラビット・ダウト, Rabitto Dauto?), est un shōnen manga écrit et dessiné par Yoshiki Tonogai", "../asset/doupt.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Gantz", "Gantz (ガンツ, Gantsu?) est un seinen manga écrit et dessiné par Hiroya Oku.", "../asset/gantz.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Ikigami", "Ikigami, préavis de mort (イキガミ?) est un manga écrit et dessiné par Motorō Mase", "../asset/ikigami.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Jeux d'enfants", "Jeux d'enfants (神さまの言うとおり, Kamisama no Iutoori?, littéralement «Comme vous l'a dit Dieu») est un shōnen manga scénarisé par Muneyuki Kaneshiro et dessiné par Akeji Fujimura", "../asset/kidgame.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Judge", "Judge (ジャッジ?) est un shōnen manga thriller et gore écrit et dessiné par Yoshiki Tonogai", "../asset/judge.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("King's Game", "King's Game (王様ゲーム, Ōsama Gēmu?) est à l'origine un roman pour téléphone portable écrit par Nobuaki Kanazawa", "../asset/KG.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("King's Game Extreme", "King's Game Extreme (王様ゲーム 終極, Ōsama Gēmu Shūkyoku?), dessinée cette fois-ci par Renji Kuriyama", "../asset/KGE.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("King's Game Origin", "King's Game Origin (王様ゲーム 起源, Ōsama Gēmu Kigen?, littéralement « Les Origines du King's Game ») et dessinée par J-Ta Yamada", "../asset/KGO.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("King's Game Spiral", "King's Game Spiral (王様ゲーム 臨場, Ōsama Gēmu Rinjō?) et dessinée à nouveau par Renji Kuriyama", "../asset/KGS.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Liar Game", "Liar Game (ライアーゲーム, Raiā Gēmu?) est un manga de Shinobu Kaitan", "../asset/liarGame.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Mirai Nikki", "Mirai Nikki (未来日記?, litt. « journal du futur ») est un manga écrit et dessiné par Sakae Esuno", "../asset/mirai.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Real Account", "Real Account (japonais リ ア ル ア カ ウ ン ト) est une série de mangas de l'auteur Okushō et de l'illustrateur Shizumu Watanabe", "../asset/real.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Parasite", "Parasite (寄生獣, Kiseijū?, litt. « bêtes parasites ») est un manga seinen de Hitoshi Iwaaki", "../asset/parasyte.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Kingdom", "Kingdom (キングダム, Kingudamu) est un manga écrit et dessiné par Yasuhisa Hara (原泰久, Hara Yasuhisa)", "../asset/kingdom.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Secret", "Secret (シークレット, Shīkuretto?) est un shōnen manga écrit et dessiné par Yoshiki Tonogai", "../asset/secret.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Ghost in the Shell", "Ghost in the Shell (攻殻機動隊, Kōkaku kidōtai?, littéralement « policiers anti-émeute en carapaces offensives »), parfois abrégé GITS, est un manga de Masamune Shirow", "../asset/GiS.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Sky High Survival", "Sky High Survival (天空侵犯, Tenkū shinpan?) est un manga de type seinen écrit par Tsuina Miura et dessiné par Takahiro Oba", "../asset/SHS.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("A Silent Voice", "A Silent Voice (聲の形, Koe no katachi?, litt. « La Forme de la voix ») est un shōnen manga de Yoshitoki Ōima", "../asset/silentVoice.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Made in Abyss", "Made in Abyss (メイドインアビス, Meido in Abisu?) est un manga écrit et dessiné par Akihito Tsukushi", "../asset/madein.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+list.push(new Manga("Akira", "Akira (アキラ?) est une série de seinen manga de science-fiction écrite et dessinée par Katsuhiro Ōtomo", "../asset/akira.jpg", "https://fr.wikipedia.org/wiki/Naruto"))
+
+
+
+function itemCreate(name = "Manga", desc = "Manga dscription", imgurl = "../asset/default.jpg", link = "#"){
+
+    let card = document.createElement("div")
+    card.classList.add("card")
+
+    let img = document.createElement("img")
+    img.setAttribute("src",imgurl)
+
+    let cardbody = document.createElement("div")
+    cardbody.classList.add("card-body")
+
+    let hcinq = document.createElement("h3")
+    hcinq.classList.add("card-title")
+    hcinq.innerText = name
+
+    let pdesc = document.createElement("p")
+    pdesc.classList.add("card-text")
+    pdesc.innerText = desc
+
+    let linkbutton = document.createElement("a")  
+    linkbutton.classList.add("btn")
+    linkbutton.classList.add("btn-primary")
+    linkbutton.setAttribute("href", link)
+    linkbutton.innerText = "Link"
+   
+
+    cardbody.appendChild(hcinq)
+    cardbody.appendChild(pdesc)
+    cardbody.appendChild(linkbutton)
+ 
+    card.appendChild(img)
+    card.appendChild(cardbody)
+
+    let currentdiv = document.querySelector(".container")
+    currentdiv.appendChild(card)
+
+}
+
+function cleancontainer(){
+    let container = document.querySelector(".container")
+    container.innerHTML = ""
+}
+
+function loading(){
+    list.forEach(element => {
+        itemCreate(element.name, element.description, element.img, element.link)
+    })
+}
+
