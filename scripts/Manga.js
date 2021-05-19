b8e331f6313faa6672f48f7af74a40924554d0a7
DelcroixFlorian <florian.delcroix98@gmail.com>
2021-05-19 20:38:01 +0200

    data

--- scripts/Manga.js
+++ scripts/Manga.js
@@ -1,8 +1,9 @@
 class Manga {
-    constructor(name, description, img, wikilink) {
+    constructor(name, description, img, wikilink, genre) {
         this.name = name
         this.description = description
         this.img = img
         this.wikilink = wikilink
+        this.genre = genre
     }
 }
