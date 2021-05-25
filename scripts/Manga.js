b00162b48ac330c317dcff57e8956bfe511aa206
DelcroixFlorian <florian.delcroix982@gmail.com>
2021-05-26 01:14:09 +0200

    json update

--- scripts/Manga.js
+++
@@ -1,9 +0,0 @@
-class Manga {
-    constructor(name, description, img, wikilink, genre) {
-        this.name = name
-        this.description = description
-        this.img = img
-        this.wikilink = wikilink
-        this.genre = genre
-    }
-}
